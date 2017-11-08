5b50f13b6b61607a0324f0aef5426acc4c8ae321
Bryan Richardson <bryanrichardson@bryans-mbp.attlocal.net>
2017-11-08 12:38:18 -0500

    Implements events screen

--- Westside/app/src/main/java/com/digicraft/westside/ui/events/EventViewModel.kt
+++ Westside/app/src/main/java/com/digicraft/westside/ui/events/EventViewModel.kt
@@ -2,34 +2,39 @@ package com.digicraft.westside.ui.events
 
 import android.content.Context
 import android.databinding.BaseObservable
+import com.digicraft.westside.R
 import com.digicraft.westside.models.Westside
+import java.text.SimpleDateFormat
+import java.util.*
 
 open class EventViewModel(val event: Westside.Event, val context: Context?) : BaseObservable() {
         val coverUrl: String?
             get() = ""
 
-    val name: String?
-        get() = "This is a Sample"
+    val title: String?
+        get() = event.title
 
-    val date: String?
-        get() = "Nov 3rd"
+    val ministry: String?
+        get() = event.groups.first().name
 
-//    val coverUrl: String?
-//        get() = event.postUser.imageUrl
-//
-//    val name: SpannableString
-//        get() {
-//            val spannableString = SpannableString(event.postText.toUpperCase())
-//            spannableString.setSpan(BackgroundColorSpan(Color.BLACK), 0, event.postText.length, SpannableString.SPAN_INCLUSIVE_INCLUSIVE)
-//            spannableString.setSpan(ForegroundColorSpan(Color.WHITE), 0, event.postText.length, SpannableString.SPAN_INCLUSIVE_INCLUSIVE)
-//            return spannableString
-//        }
-//
-//    val date: SpannableString
-//        get() {
-//            val spannableString = SpannableString(event.createdDate)
-//            spannableString.setSpan(BackgroundColorSpan(Color.BLACK), 0, event.createdDate.length, SpannableString.SPAN_INCLUSIVE_INCLUSIVE)
-//            spannableString.setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.colorAccent)), 0, event.createdDate.length, SpannableString.SPAN_INCLUSIVE_INCLUSIVE)
-//            return spannableString
-//        }
+    val description: String?
+        get() = event.description
+
+    val day: String?
+        get() {
+            val formatter = SimpleDateFormat("dd", Locale.US)
+            return formatter.format(event.startTime)
+        }
+
+    val month: String?
+        get() {
+            val formatter = SimpleDateFormat("MMM", Locale.US)
+            return formatter.format(event.startTime)
+        }
+
+    val time: String?
+        get() {
+            val formatter = SimpleDateFormat("hh:mm a", Locale.US)
+            return formatter.format(event.startTime)
+        }
 }
